a0710a128b7c1705a0d589441b342c76bf5cba45
Roland Leißa <leissa@uni-mannheim.de>
2022-09-04 00:33:09 +0200

    missing file + test

---
+++ pos.py
@@ -0,0 +1,10 @@
+class Pos:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+class Loc:
+    def __init__(self, file, begin, finis):
+        self.file = file
+        self.begin = begin
+        self.finis = finis
