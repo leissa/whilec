c2fc8a31e2ca025fdec1f5292d583a179223c4ae
Roland Leißa <leissa@uni-mannheim.de>
2022-09-04 00:30:36 +0200

    wip: cli + lexing

--- lexer.py
+++ lexer.py
@@ -1,27 +1,74 @@
-from tok import *
+import string
+from enum import Enum, auto
+from pos import *
+
+class Tok:
+    class Tag(Enum):
+        # keywords
+        K_while     = auto(),
+        # misc
+        M_id        = auto(),
+        M_eof       = auto(),
+        # further tokens
+        T_add       = auto(),
+        T_sub       = auto(),
+        T_assign    = auto(),
+        T_semicolon = auto(),
+
+        def __str__(self):
+            if self is Tok.Tag.K_while:
+                return 'while'
+            if self is Tok.Tag.M_id:
+                return '<identifier>'
+            if self is Tok.Tag.T_add:
+                return '+'
+            if self is Tok.Tag.T_sub:
+                return '-'
+            if self is Tok.Tag.T_assign:
+                return '='
+            if self is Tok.Tag.T_semicolon:
+                return ';'
+            assert False
+
+    def __init__(self, loc, tag):
+        self.loc = loc
+        self.tag = tag
+        print(f'tag: {self.tag}')
 
 class Lexer:
     def __init__(self, filename):
         self.file = open(filename, "r")
 
-    def accept(self, item):
+    def accept_if(self, pred):
         tell = self.file.tell()
-        data = self.file.read(len(item))
+        char = self.file.read(1)
 
-        if data == item:
-            self.str_ += data
+        if pred(char):
+            self.str += char
             return True;
 
-        self.file.seek(tell)
+        self.file.seek(tell) # undo read
         return False;
 
+    def accept(self, char):
+        return self.accept_if(lambda c : c == char)
+
     def lex(self):
-        self.str_ = ''
-        self.pos_ = Pos(1, 1)
+        self.pos = Pos(1, 1)
 
         while True:
-            if self.accept("+"):
-                return Tok(Tok.Tag.T_add, pos_)
-            if self.accept("-"):
-                return Tok(Tok.Tag.T_sub, pos_)
+            self.str = ''
+
+            if self.accept_if(lambda char : char in string.whitespace):
+                continue
+            if self.accept('+'):
+                return Tok(self.pos, Tok.Tag.T_add)
+            if self.accept('-'):
+                return Tok(self.pos, Tok.Tag.T_sub)
+            if self.accept('='):
+                return Tok(self.pos, Tok.Tag.T_assign)
+            if self.accept(';'):
+                return Tok(self.pos, Tok.Tag.T_semicolon)
+
+            print('error')
             break
--- while.py
+++ while.py
@@ -5,9 +5,11 @@ from parser import Parser
 
 cli = argparse.ArgumentParser(description='Compiler and interpreter for the while languge.')
 
-cli.add_argument('input', metavar='file', type=str, help='input file')
-cli.add_argument('--eval', dest='eval', help='evaluate (interpret) while program')
+
+cli.add_argument('--eval',    dest='eval',    action='store_true', help='interpret input program')
+cli.add_argument('--emit-c',  dest='emit_c',  action='store_true', help='compile input program to C')
+cli.add_argument('--emit-py', dest='emit_py', action='store_true', help='compile input program to Python')
+cli.add_argument('file',                                           help='input file')
 
 args = cli.parse_args()
-
-parser = Parser(args.input)
+parser = Parser(args.file)
