49e80b5909d1352b4654823d2c8bd755fbbd4242
Roland Leißa <leissa@uni-mannheim.de>
2022-09-03 16:45:04 +0200

    wip: lexing

--- lexer.py
+++ lexer.py
@@ -1,7 +1,27 @@
+from tok import *
+
 class Lexer:
     def __init__(self, filename):
         self.file = open(filename, "r")
 
-    def lex():
+    def accept(self, item):
+        tell = self.file.tell()
+        data = self.file.read(len(item))
+
+        if data == item:
+            self.str_ += data
+            return True;
+
+        self.file.seek(tell)
+        return False;
+
+    def lex(self):
+        self.str_ = ''
+        self.pos_ = Pos(1, 1)
+
         while True:
-            str = ''
+            if self.accept("+"):
+                return Tok(Tok.Tag.T_add, pos_)
+            if self.accept("-"):
+                return Tok(Tok.Tag.T_sub, pos_)
+            break
--- parser.py
+++ parser.py
@@ -3,3 +3,4 @@ from lexer import Lexer
 class Parser:
     def __init__(self, filename):
         self.lexer = Lexer(filename)
+        self.lexer.lex()
