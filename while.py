884890771fab99ff5736fe76e9dc5af4e894ac66
Roland Leißa <leissa@uni-mannheim.de>
2022-09-05 16:34:05 +0200

    added print flag

--- while.py
+++ while.py
@@ -10,9 +10,12 @@ cli = argparse.ArgumentParser(description="Compiler and interpreter for the whil
 cli.add_argument("--eval",    dest="eval",    action="store_true", help="interpret input program")
 cli.add_argument("--emit-c",  dest="emit_c",  action="store_true", help="compile input program to C")
 cli.add_argument("--emit-py", dest="emit_py", action="store_true", help="compile input program to Python")
+cli.add_argument("--print",   dest="print",   action="store_true", help="print program again")
 cli.add_argument("file",                                           help="input file")
 
 args   = cli.parse_args()
 parser = Parser(args.file)
 prog   = parser.parse_prog()
-print(prog)
+
+if args.print:
+    print(prog)
